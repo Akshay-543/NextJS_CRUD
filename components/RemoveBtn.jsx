b1775f7a0af3509b0320b9b723c3fee08eb57610
2139386 <140402641+2139386@users.noreply.github.com>
2024-11-06 14:12:03 +0530

    inital commit for nextjs crud app

---
+++ components/RemoveBtn.jsx
@@ -0,0 +1,9 @@
+import { HiOutlineTrash } from "react-icons/hi";
+
+export default function RemoveBtn() {
+  return (
+    <button>
+      <HiOutlineTrash className="text-2xl text-red-500" />
+    </button>
+  );
+}
